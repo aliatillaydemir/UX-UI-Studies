89ac7e9337acaadcd44b6165e00799b9e27f3c64
Atilla Aydemir <atilla-734@hotmail.com>
2022-07-12 04:28:39 +0300

    " "

---
+++ app/src/main/java/com/ayd/floatbottomnav/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ayd.floatbottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ayd.floatbottomnav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavView.background = null //ignore the shadow
+        binding.bottomNavView.menu.getItem(2).isEnabled = false
+
+
+    }
+}
